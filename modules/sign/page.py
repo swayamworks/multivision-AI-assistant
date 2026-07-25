22ba97919a50409742ae6c28d28d34567935118a
Swayam <swayam@example.com>
2026-07-25 17:38:57 +0530

    fix(webrtc): Update OpenRelay TURN authentication credentials to openrelayproject

--- modules/sign/page.py
+++ modules/sign/page.py
@@ -313,17 +313,20 @@ def render_realtime_mode(model):
                         "iceServers": [
                             {"urls": ["stun:stun.l.google.com:19302"]},
                             {"urls": ["stun:stun1.l.google.com:19302"]},
-                            {"urls": ["stun:stun2.l.google.com:19302"]},
-                            {"urls": ["stun:stun3.l.google.com:19302"]},
-                            {"urls": ["stun:stun4.l.google.com:19302"]},
                             {
-                                "urls": [
-                                    "turn:openrelay.metered.ca:80",
-                                    "turn:openrelay.metered.ca:443",
-                                    "turn:openrelay.metered.ca:443?transport=tcp",
-                                ],
-                                "username": "openrelay",
-                                "credential": "openrelay",
+                                "urls": ["turn:openrelay.metered.ca:80"],
+                                "username": "openrelayproject",
+                                "credential": "openrelayproject",
+                            },
+                            {
+                                "urls": ["turn:openrelay.metered.ca:443"],
+                                "username": "openrelayproject",
+                                "credential": "openrelayproject",
+                            },
+                            {
+                                "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
+                                "username": "openrelayproject",
+                                "credential": "openrelayproject",
                             },
                         ]
                     }
